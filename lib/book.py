c4727f1f9b8f20eaf45c125c6bc081b6ff989633
sidee <said.issack@student.moringaschool.com>
2023-08-21 23:39:51 +0300

    completed assighnment

--- lib/book.py
+++ lib/book.py
@@ -3,4 +3,28 @@
 class Book:
     pass
     
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = None
+        self.page_count = page_count
+
+    def get_page_count(self):
+        return self._page_count
+
+    def set_page_count(self, page_count):
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    page_count = property(get_page_count, set_page_count)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+
+book1= Book('Test', 123)
+book1.page_count = 12
+book1.turn_page()
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -2,3 +2,27 @@
 
 class Shoe:
     pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = None
+        self.size = size
+
+    def get_size(self):
+        return self._size
+
+    def set_size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print('size must be an integer')  
+
+    size = property(get_size, set_size)
+
+    def cobble(self):
+        print('Your shoe is as good as new!')   
+        self.condition = 'New'           
+
+
+shoe1 = Shoe('test', 9)   
+shoe1.size = 'cada'   
+shoe1.cobble()          
